eba33b0f37542e248591584bd9a4b001f9109f45
Azad <linuxoracledev@gmail.com>
2022-01-29 09:13:50 +0600

    first commit

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent implements OnInit {
+
+  constructor() { }
+property:any={
+  id:1,
+  name:"Birla",
+  type:"House",
+  price:1500,
+  imageUrl:"assets/images/house-default.png"
+}
+  ngOnInit(): void {
+  }
+
+}
